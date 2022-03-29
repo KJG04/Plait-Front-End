a0b11b85789d20d59614bff13667bc53f5192fcd
김진근 <freedom7113@gmail.com>
2022-03-29 22:13:59 +0900

    🔌 - add provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,21 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ThemeProvider } from "@emotion/react";
+import { theme } from "../src/utils/theme";
+import { Toaster } from "react-hot-toast";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+        <Toaster position="top-center" />
+      </ThemeProvider>
+    </QueryClientProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
