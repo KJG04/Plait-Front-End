fd8c57d7d1c228fa68d5bb195d1539817af65b98
김진근 <freedom7113@gmail.com>
2022-03-30 10:46:44 +0900

    🔌 (#2) - add text in main

--- src/containers/Main/index.tsx
+++ src/containers/Main/index.tsx
@@ -1,7 +1,16 @@
 import { NextPage } from "next";
+import * as S from "./styles";
 
 const MainContainer: NextPage = () => {
-  return <div>hello world this is main</div>;
+  return (
+    <S.Container>
+      <S.Title>Plait에 오신걸 환영합니다!</S.Title>
+      <S.Subtitle>
+        <div>현재 123명이 Plait을 사용중입니다.</div>
+        <div>사람들과 실시간으로 음악과 동영상을 공유해보세요.</div>
+      </S.Subtitle>
+    </S.Container>
+  );
 };
 
 export default MainContainer;
--- src/containers/Main/styles.ts
+++ src/containers/Main/styles.ts
@@ -1,1 +1,27 @@
-export {};
+import styled from "@emotion/styled";
+
+export const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+export const Title = styled.div`
+  font: ${({ theme }) => theme.fonts.heading1};
+  color: ${({ theme }) => theme.colors.grayscale.white};
+  text-align: center;
+`;
+
+export const Subtitle = styled.div`
+  font: ${({ theme }) => theme.fonts.heading3};
+  color: ${({ theme }) => theme.colors.grayscale.darkWhite};
+  text-align: center;
+  margin-top: 16px;
+`;
+
+export const Input = styled.div`
+  
+`;
