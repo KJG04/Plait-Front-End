92143f7e9f4386e308c03a01a23ed9e4f1e80e33
김진근 <freedom7113@gmail.com>
2022-03-30 10:46:55 +0900

    😎 (#2) - fix theme import path

--- src/utils/theme/emotion.d.ts
+++ src/utils/theme/emotion.d.ts
@@ -1,5 +1,5 @@
 import "@emotion/react";
-import { Theme as ThemeType } from "./theme";
+import { Theme as ThemeType } from "./index";
 
 declare module "@emotion/react" {
   export interface Theme extends ThemeType {}
