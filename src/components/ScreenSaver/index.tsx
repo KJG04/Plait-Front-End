249a8663e25585c28d5c33e23e3934c5030df0c6
김진근 <freedom7113@gmail.com>
2022-03-30 11:29:34 +0900

    🔌 (#2) - add screen saver component

---
+++ src/components/ScreenSaver/index.tsx
@@ -0,0 +1,7 @@
+import * as S from "./styles";
+
+const ScreenSaver = () => {
+  return <S.Canvas></S.Canvas>;
+};
+
+export default ScreenSaver;
---
+++ src/components/ScreenSaver/styles.ts
@@ -0,0 +1,10 @@
+import styled from "@emotion/styled";
+
+export const Canvas = styled.canvas`
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  width: 100vw;
+  height: 100vh;
+  z-index: 1;
+`;
