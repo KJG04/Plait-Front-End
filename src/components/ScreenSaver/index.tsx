fc1b3413c6f28ca5522d149b5cb3fa763f227b09
김진근 <freedom7113@gmail.com>
2022-04-04 10:44:00 +0900

    🔌 (#2) - can drag object

--- src/components/ScreenSaver/index.tsx
+++ src/components/ScreenSaver/index.tsx
@@ -1,23 +1,47 @@
 import React, { useCallback, useEffect, useRef } from "react";
-import { Engine, Render, Bodies, World, Body, Vector } from "matter-js";
+import {
+  Engine,
+  Render,
+  Bodies,
+  World,
+  Body,
+  Runner,
+  IChamferableBodyDefinition,
+  Common,
+  MouseConstraint,
+  Mouse,
+  IMouseConstraintDefinition,
+  Constraint,
+} from "matter-js";
 import * as S from "./styles";
 import { useTheme } from "@emotion/react";
 
 const ScreenSaver = () => {
   const scene = useRef<HTMLDivElement>(null);
   const engine = useRef<Engine>(Engine.create());
+  const render = useRef<Render | undefined>(undefined);
   const theme = useTheme();
 
-  const doFirst = useCallback(() => {
+  const cleanUp = useCallback(() => {
+    if (render.current) {
+      Render.stop(render.current);
+      World.clear(engine.current.world, false);
+      Engine.clear(engine.current);
+      render.current.canvas.remove();
+      render.current.textures = {};
+    }
+  }, []);
+
+  useEffect(() => {
     if (!scene.current) {
-      return;
+      return () => {};
     }
 
     const cw = document.body.clientWidth;
     const ch = document.body.clientHeight;
 
-    const render = Render.create({
-      element: scene.current,
+    render.current = Render.create({
+      element: scene.current || undefined,
       engine: engine.current,
       options: {
         width: cw,
@@ -27,44 +51,63 @@ const ScreenSaver = () => {
       },
     });
 
-    World.add(engine.current.world, [
-      Bodies.rectangle(cw / 2, -10, cw, 20, { isStatic: true }),
-      Bodies.rectangle(-10, ch / 2, 20, ch, { isStatic: true }),
-      Bodies.rectangle(cw / 2, ch + 10, cw, 20, { isStatic: true }),
-      Bodies.rectangle(cw + 10, ch / 2, 20, ch, { isStatic: true }),
-    ]);
-    engine.current.gravity.scale = 0;
+    const option: IChamferableBodyDefinition = {
+      isStatic: true,
+      density: 1,
+      render: {
+        opacity: 0,
+      },
+    };
 
-    Engine.run(engine.current);
-    Render.run(render);
+    const size = 20;
 
-    const ball = Bodies.circle(100, 200, 10 + Math.random() * 30, {
-      mass: 1,
-      restitution: 1,
-      friction: 0,
+    const top = Bodies.rectangle(0, -size, cw * 2, size, option);
+    const left = Bodies.rectangle(-size, 0, size, ch * 2, option);
+    const bottom = Bodies.rectangle(0, ch, cw * 2, size, option);
+    const right = Bodies.rectangle(cw, 0, size, ch * 2, option);
+
+    World.add(engine.current.world, [top, right, bottom, left]);
+    engine.current.gravity.x = 0;
+    engine.current.gravity.y = 0;
+
+    Runner.run(engine.current);
+    Render.run(render.current);
+
+    const ball = Bodies.circle(100, 200, 30 + Math.random() * 30, {
+      restitution: 0.8,
+      friction: 0.01,
       frictionAir: 0,
-      frictionStatic: 0,
+      density: 1,
+      inertia: Infinity,
       render: {
         fillStyle: theme.colors.primary,
       },
     });
 
-    Body.setVelocity(ball, Vector.create(1, 1));
+    const mouse = Mouse.create(render.current.canvas);
+
+    const options: IMouseConstraintDefinition = {
+      mouse,
+    };
+
+    const constraints = MouseConstraint.create(engine.current, options);
+    constraints.constraint.stiffness = 0.001;
+    constraints.constraint.render.visible = false;
+    World.add(engine.current.world, constraints);
+
+    var forceMagnitude = 0.001 * ball.mass;
+
+    Body.applyForce(ball, ball.position, {
+      x:
+        (forceMagnitude + Common.random() * forceMagnitude) *
+        Common.choose([1, -1]),
+      y: -forceMagnitude + Common.random() * -forceMagnitude,
+    });
 
     World.add(engine.current.world, [ball]);
 
-    return () => {
-      Render.stop(render);
-      World.clear(engine.current.world, false);
-      Engine.clear(engine.current);
-      render.canvas.remove();
-      render.textures = {};
-    };
-  }, [theme]);
-
-  useEffect(() => {
-    doFirst();
-  }, [doFirst]);
+    return cleanUp;
+  }, [cleanUp, theme]);
 
   return <S.Canvas ref={scene} />;
 };
