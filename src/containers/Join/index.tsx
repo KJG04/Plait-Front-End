53501756f75df5f13cf3a6748f73b301b03be85b
김진근 <freedom7113@gmail.com>
2022-04-11 12:05:21 +0900

    ➕️ (#4) - add join container

---
+++ src/containers/Join/index.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from "next";
+
+const JoinContainer: NextPage = () => {
+  return <div>this is join</div>;
+};
+
+export default JoinContainer;
--- src/containers/index.ts
+++ src/containers/index.ts
@@ -1,2 +1,3 @@
 export { default as MainContainer } from "./Main";
 export { default as RoomContainer } from "./Room";
+export { default as JoinContainer } from "./Join";
