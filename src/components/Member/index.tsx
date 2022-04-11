7424734758051fdc8a73ea6559636fb0dc6663cd
김진근 <freedom7113@gmail.com>
2022-04-11 17:11:19 +0900

    🔌 (#4) - add member component

---
+++ src/components/Member/index.tsx
@@ -0,0 +1,12 @@
+import * as S from "./styles";
+
+const Member = () => {
+  return (
+    <S.Contianer>
+      <S.Profile />
+      <span>kjg2004</span>
+    </S.Contianer>
+  );
+};
+
+export default Member;
---
+++ src/components/Member/styles.ts
@@ -0,0 +1,25 @@
+import styled from "@emotion/styled";
+
+export const Contianer = styled.button`
+  padding: 8px 10px;
+  border-radius: 10px;
+  border: none;
+  margin: none;
+  display: flex;
+  transition: background-color 0.15s ease-in-out;
+  background-color: transparent;
+  color: ${({ theme }) => theme.colors.grayscale.white};
+  column-gap: 12px;
+  align-items: center;
+
+  &:hover {
+    background-color: ${({ theme }) => theme.colors.grayscale.gray};
+  }
+`;
+
+export const Profile = styled.div`
+  border-radius: 50%;
+  width: 36px;
+  height: 36px;
+  background-color: ${({ theme }) => theme.colors.grayscale.lightGray};
+`;
