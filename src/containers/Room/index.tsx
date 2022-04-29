349dfab3d91b3e8fbb4e9e224ee565cd5e43d0a1
김진근 <freedom7113@gmail.com>
2022-04-29 22:49:20 +0900

    ➕️ (#4) - add player component

--- src/containers/Room/index.tsx
+++ src/containers/Room/index.tsx
@@ -2,6 +2,7 @@ import { NextPage } from "next";
 import Aside from "../../components/Aside";
 import BottomBar from "../../components/BottomBar";
 import Members from "../../components/Members";
+import Player from "../../components/Player";
 import * as S from "./styles";
 
 const RoomContainer: NextPage = () => {
@@ -9,6 +10,7 @@ const RoomContainer: NextPage = () => {
     <S.Container>
       <S.TopContainer>
         <Aside />
+        <Player />
       </S.TopContainer>
       <BottomBar />
       <Members />
