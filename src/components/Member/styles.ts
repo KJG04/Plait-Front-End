91a532ea7793201b787bd81081bb1050d3a7f3f2
김진근 <freedom7113@gmail.com>
2022-04-30 21:13:15 +0900

    ➕️ (#4) - add member component

---
+++ src/components/Member/styles.ts
@@ -0,0 +1,8 @@
+import styled from "@emotion/styled";
+
+export const Profile = styled.div`
+  width: 36px;
+  height: 36px;
+  background-color: ${({ theme }) => theme.colors.grayscale.lightGray};
+  border-radius: 50%;
+`;
