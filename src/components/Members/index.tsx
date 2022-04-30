108317627bf5934e4764eef029b345eee61aa436
김진근 <freedom7113@gmail.com>
2022-04-30 21:13:39 +0900

    🔌 (#4) - add hover action in member container

--- src/components/Members/index.tsx
+++ src/components/Members/index.tsx
@@ -1,15 +1,39 @@
+import { memo, useCallback, useEffect, useMemo, useState } from "react";
+import Member from "../Member";
 import * as S from "./styles";
 
 const Members = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const onMouseMove = useCallback((e: MouseEvent) => {
+    const { clientY: y } = e;
+
+    if (y > 0 && y < 70) {
+      setIsOpen(true);
+    } else {
+      setIsOpen(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    document.addEventListener("mousemove", onMouseMove);
+
+    return () => {
+      document.addEventListener("mousemove", onMouseMove);
+    };
+  }, [onMouseMove]);
+
+  const containerClassName = useMemo(() => (!isOpen ? "close" : ""), [isOpen]);
+
   return (
-    <S.Container>
+    <S.Container className={containerClassName}>
       {Array(4)
         .fill(0)
         .map((_, index) => (
-          <S.Profile key={index} />
+          <Member key={index} />
         ))}
     </S.Container>
   );
 };
 
-export default Members;
+export default memo(Members);
--- src/components/Members/styles.ts
+++ src/components/Members/styles.ts
@@ -10,11 +10,9 @@ export const Container = styled.div`
   flex-wrap: wrap;
   justify-content: right;
   gap: 8px;
-`;
+  transition: all 1s cubic-bezier(0.075, 0.82, 0.165, 1);
 
-export const Profile = styled.div`
-  width: 36px;
-  height: 36px;
-  background-color: ${({ theme }) => theme.colors.grayscale.lightGray};
-  border-radius: 50%;
+  &.close {
+    transform: translateY(-100%);
+  }
 `;
