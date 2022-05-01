177cb233c36708a7ceee3f34b87c77e8778f7619
김진근 <freedom7113@gmail.com>
2022-05-01 20:53:50 +0900

    🔌 (#4) - implement floating emoji feature

--- src/components/FloatingEmoji/index.tsx
+++ src/components/FloatingEmoji/index.tsx
@@ -1,13 +1,72 @@
-import { FC } from "react";
-import { FloatingEmojiType } from "../../atoms/floatingEmojis";
+import gsap, { Elastic, Linear, Power4 } from "gsap";
+import { FC, memo, useCallback, useEffect, useRef } from "react";
+import { useSetRecoilState } from "recoil";
+import floatingEmojisState, {
+  FloatingEmojiType,
+} from "../../atoms/floatingEmojis";
+import * as S from "./styles";
 
 const FloatingEmoji: FC<FloatingEmojiType> = ({
   emoji,
   id,
-  xPercent,
-  yPercent,
+  x,
+  y,
+  color,
+  name,
 }) => {
-  return <div></div>;
+  const containerRef = useRef<HTMLDivElement>(null);
+  const setFloatingEmojis = useSetRecoilState(floatingEmojisState);
+
+  const anim = useCallback(() => {
+    if (!containerRef.current) {
+      return;
+    }
+
+    const tl = gsap.timeline();
+
+    tl.from(
+      containerRef.current,
+      {
+        duration: 1,
+        scale: 0,
+        opacity: 0,
+        ease: Elastic.easeOut.config(1, 0.5),
+      },
+      0,
+    )
+      .to(
+        containerRef.current,
+        {
+          duration: 3,
+          y: -100,
+          ease: Linear.easeNone,
+        },
+        "<",
+      )
+      .to(
+        containerRef.current,
+        {
+          scale: 0,
+          duration: 0.5,
+          ease: Power4.easeIn,
+          onComplete: () => {
+            setFloatingEmojis((prev) => prev.filter((v) => v.id !== id));
+          },
+        },
+        2.5,
+      );
+  }, [id, setFloatingEmojis]);
+
+  useEffect(() => {
+    anim();
+  }, [anim]);
+
+  return (
+    <S.Container ref={containerRef} style={{ top: `${y}px`, left: `${x}px` }}>
+      <S.Emoji color={color}>{emoji}</S.Emoji>
+      <S.Name color={color}>{name}</S.Name>
+    </S.Container>
+  );
 };
 
-export default FloatingEmoji;
+export default memo(FloatingEmoji);
