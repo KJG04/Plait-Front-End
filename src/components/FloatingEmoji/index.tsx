b5abdf2bdb6d05da69d2780f95a6083f3d6a4b74
김진근 <freedom7113@gmail.com>
2022-05-01 16:48:51 +0900

    🔌 (#4) - add floating emoji component

---
+++ src/components/FloatingEmoji/index.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+import { FloatingEmojiType } from "../../atoms/floatingEmojis";
+
+const FloatingEmoji: FC<FloatingEmojiType> = ({
+  emoji,
+  id,
+  xPercent,
+  yPercent,
+}) => {
+  return <div></div>;
+};
+
+export default FloatingEmoji;
