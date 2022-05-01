74b2057d71d62ea898a397515c76cc8c7029ac4b
김진근 <freedom7113@gmail.com>
2022-05-01 16:49:13 +0900

    🔌 (#4) - implement emoji event listener

--- src/components/EmojiEventListener/index.tsx
+++ src/components/EmojiEventListener/index.tsx
@@ -1,12 +1,35 @@
+import React, { memo, useMemo } from "react";
 import { useCallback, useEffect } from "react";
+import { useRecoilState } from "recoil";
+import floatingEmojisState, {
+  FloatingEmojiType,
+} from "../../atoms/floatingEmojis";
 import { EmojiEvent, emojiEventName } from "../../constant/emojiEvent";
+import FloatingEmoji from "../FloatingEmoji";
+import * as S from "./styles";
+import uuid from "uuid";
 
 const EmojiEventListener = () => {
-  const emojiListener = useCallback((e: Event) => {
-    if (!(e instanceof EmojiEvent)) {
-      return;
-    }
-  }, []);
+  const [floatingEmojis, setFloatingEmojis] =
+    useRecoilState(floatingEmojisState);
+
+  const emojiListener = useCallback(
+    (e: Event) => {
+      if (!(e instanceof EmojiEvent)) {
+        return;
+      }
+
+      const newEmoji: FloatingEmojiType = {
+        emoji: e.emoji,
+        id: uuid.v4(),
+        xPercent: e.xPercent,
+        yPercent: e.yPercent,
+      };
+
+      setFloatingEmojis((prev) => [...prev, newEmoji]);
+    },
+    [setFloatingEmojis],
+  );
 
   useEffect(() => {
     document.addEventListener(emojiEventName, emojiListener);
@@ -16,7 +39,15 @@ const EmojiEventListener = () => {
     };
   }, [emojiListener]);
 
-  return <></>;
+  const renderFloatingEmojis = useMemo(
+    () =>
+      floatingEmojis.map((value) => (
+        <FloatingEmoji {...value} key={value.id} />
+      )),
+    [floatingEmojis],
+  );
+
+  return <S.Container>{renderFloatingEmojis}</S.Container>;
 };
 
-export default EmojiEventListener;
+export default memo(EmojiEventListener);
--- src/components/EmojiEventListener/styles.ts
+++ src/components/EmojiEventListener/styles.ts
@@ -1,1 +1,9 @@
-export {};
+import styled from "@emotion/styled";
+
+export const Container = styled.div`
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  width: 100vw;
+  height: 100vh;
+`;
