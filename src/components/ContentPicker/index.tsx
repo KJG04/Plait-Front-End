ac6210af07f588e523d0d9e5b20a8900cdf27ec3
김진근 <freedom7113@gmail.com>
2022-05-02 11:20:57 +0900

    🔌 (#4) - add content picker component

---
+++ src/components/ContentPicker/index.tsx
@@ -0,0 +1,8 @@
+import { memo } from "react";
+import * as S from "./styles";
+
+const ContentPicker = () => {
+  return <S.Container>hello world</S.Container>;
+};
+
+export default memo(ContentPicker);
---
+++ src/components/ContentPicker/styles.ts
@@ -0,0 +1,7 @@
+import styled from "@emotion/styled";
+
+export const Container = styled.div`
+  background-color: ${({ theme }) => theme.colors.grayscale.black};
+  border-radius: 20px;
+  padding: 24px;
+`;
