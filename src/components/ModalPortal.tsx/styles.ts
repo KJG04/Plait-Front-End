ca73a54b3cc39626336994ff91cd85b90d5d9b0b
김진근 <freedom7113@gmail.com>
2022-05-02 10:18:06 +0900

    🔌 (#4) - change modal backdrop style

--- src/components/ModalPortal.tsx/styles.ts
+++ src/components/ModalPortal.tsx/styles.ts
@@ -17,6 +17,6 @@ export const Backdrop = styled.div`
   left: 0px;
   width: 100%;
   height: 100%;
-  background-color: ${({ theme }) => theme.colors.grayscale.black};
-  opacity: 0.6;
+  background-color: #000;
+  opacity: 0.5;
 `;
