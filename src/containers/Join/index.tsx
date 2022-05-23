98a9f85509d517b50ee7011ac65ad611adb7487a
김진근 <freedom7113@gmail.com>
2022-05-23 09:03:20 +0900

    💣 (#5) - remove join page

--- src/containers/Join/index.tsx
+++
@@ -1,7 +0,0 @@
-import { NextPage } from "next";
-
-const JoinContainer: NextPage = () => {
-  return <div>this is join</div>;
-};
-
-export default JoinContainer;
--- src/containers/index.ts
+++ src/containers/index.ts
@@ -1,4 +1,3 @@
 export { default as MainContainer } from "./Main";
 export { default as RoomContainer } from "./Room";
-export { default as JoinContainer } from "./Join";
 export { default as Container404 } from "./404";
