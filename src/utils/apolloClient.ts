1cba42bf98c618aa6fb018574d54fdab8dcd393a
김진근 <freedom7113@gmail.com>
2022-05-25 23:25:55 +0900

    🔌 (#7) - can use subscription on apollo client

--- src/utils/apolloClient.ts
+++ src/utils/apolloClient.ts
@@ -1,9 +1,35 @@
-import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
+import { ApolloClient, HttpLink, InMemoryCache, split } from "@apollo/client";
+import { getMainDefinition } from "@apollo/client/utilities";
+import { WebSocketLink } from "@apollo/client/link/ws";
+import { SubscriptionClient } from "subscriptions-transport-ws";
 
-const link = createHttpLink({
+const wsLink = process.browser
+  ? new WebSocketLink(
+      new SubscriptionClient("ws://localhost:8080/graphql", {
+        reconnect: true,
+      }),
+    )
+  : null;
+
+const httplink = new HttpLink({
   uri: "http://localhost:8080/graphql",
   credentials: "include",
 });
+
+const link = process.browser
+  ? split(
+      //only create the split in the browser
+      // split based on operation type
+      ({ query }) => {
+        const def = getMainDefinition(query);
+        return (
+          def.kind === "OperationDefinition" && def.operation === "subscription"
+        );
+      },
+      wsLink as WebSocketLink,
+      httplink,
+    )
+  : httplink;
 
 const apolloClient = new ApolloClient({
   link,
