98268616090f851895472fb25676ba6f49206f64
김진근 <freedom7113@gmail.com>
2022-06-08 14:09:51 +0900

    🔌 (#7) - update mobile cover style

--- src/components/MobileCover/styles.ts
+++ src/components/MobileCover/styles.ts
@@ -8,14 +8,19 @@ export const Container = styled.div`
   align-items: center;
   flex-direction: column;
   row-gap: 16px;
+  position: absolute;
+  top: 0px;
+  left: 0px;
 `;
 
 export const Title = styled.div`
   font: ${({ theme }) => theme.fonts.heading2};
   color: ${({ theme }) => theme.colors.grayscale.white};
+  text-align: center;
 `;
 
 export const Description = styled.div`
   font: ${({ theme }) => theme.fonts.body2};
   color: ${({ theme }) => theme.colors.grayscale.lightGray};
+  text-align: center;
 `;
